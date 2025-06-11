146ec2c6173f7b84c2e061da00f483af76ee63ad
Carddera1842 <carddera1842@gmail.com>
2025-06-11 12:01:16 -0500

    Demo: Creating an Object

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,2 +1,18 @@
 ﻿
 
+using BethanysPieShopHRM;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("----------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+
+double recieveWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from Program): {recieveWageBethany}");
