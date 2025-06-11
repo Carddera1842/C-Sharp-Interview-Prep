9418852f577523ad21b76ed21dbcfd5f2999ebfc
Carddera1842 <carddera1842@gmail.com>
2025-06-11 10:14:13 -0500

    Demo: Using Optional Parameters and Named Arguments

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -6,16 +6,18 @@ int amount = 1234;
 int months = 12;
 int bonus = 1000;
 
-int yearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
+//int yearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
 
-Console.WriteLine($"Yearly wage: {yearlyWage}");
+//Console.WriteLine($"Yearly wage: {yearlyWage}");
 
-Console.ReadLine();
+//Console.ReadLine();
 
-double amountDouble = 1500.0;
-double monthsDouble = 12;
-double bonusDouble = 1000;
+//double amountDouble = 1500.0;
+//double monthsDouble = 12;
+//double bonusDouble = 1000;
 
-double yearlyWageDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+//double yearlyWageDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+Utilities.UsingOptionalParamenters();
 
 Console.ReadLine();
