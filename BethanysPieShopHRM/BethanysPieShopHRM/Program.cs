1d2b1b4d8e01090fe11b97adb169add843eec800
Carddera1842 <carddera1842@gmail.com>
2025-06-11 12:27:41 -0500

    Demo: Working with Value Types and Reference Types

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -2,45 +2,57 @@
 
 using BethanysPieShopHRM;
 
+//int a = 42;
+//int aCopy = a;
+//Console.WriteLine($"Value of a: {a} and value of a copy of a: {aCopy}");
+//aCopy = 100;
+//Console.WriteLine($"Value of a: {a} and value of a copy of a: {aCopy}");
+
 Console.WriteLine("Creating an employee");
 Console.WriteLine("----------------------\n");
 
 Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
 
+Employee testEmployee = bethany;
+testEmployee.firstName = "Gill";
+
+testEmployee.DisplayEmployeeDetails();
 bethany.DisplayEmployeeDetails();
 
-bethany.PerformWork();
-bethany.PerformWork();
-bethany.PerformWork(5);
-bethany.PerformWork();
+//bethany.DisplayEmployeeDetails();
 
-bethany.firstName = "John";
-bethany.hourlyRate = 10;
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork(5);
+//bethany.PerformWork();
 
-bethany.DisplayEmployeeDetails();
+//bethany.firstName = "John";
+//bethany.hourlyRate = 10;
 
-bethany.PerformWork();
-bethany.PerformWork(12);
-bethany.PerformWork();
-bethany.PerformWork();
+//bethany.DisplayEmployeeDetails();
 
-string fn = bethany.firstName;
+//bethany.PerformWork();
+//bethany.PerformWork(12);
+//bethany.PerformWork();
+//bethany.PerformWork();
 
-double recieveWageBethany = bethany.ReceiveWage(true);
-Console.WriteLine($"Wage paid (message from Program): {recieveWageBethany}");
+//string fn = bethany.firstName;
 
-Console.WriteLine("Creating an employee");
-Console.WriteLine("----------------------\n");
+//double recieveWageBethany = bethany.ReceiveWage(true);
+//Console.WriteLine($"Wage paid (message from Program): {recieveWageBethany}");
 
-Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
+//Console.WriteLine("Creating an employee");
+//Console.WriteLine("----------------------\n");
 
-george.DisplayEmployeeDetails();
+//Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30);
 
-george.PerformWork();
-george.PerformWork();
-george.PerformWork(3);
-george.PerformWork();
-george.PerformWork(8);
+//george.DisplayEmployeeDetails();
 
-double recieveWageGeorge = george.ReceiveWage(true);
-Console.WriteLine($"Wage paid (message from Program): {recieveWageGeorge}");
+//george.PerformWork();
+//george.PerformWork();
+//george.PerformWork(3);
+//george.PerformWork();
+//george.PerformWork(8);
+
+//double recieveWageGeorge = george.ReceiveWage(true);
+//Console.WriteLine($"Wage paid (message from Program): {recieveWageGeorge}");
