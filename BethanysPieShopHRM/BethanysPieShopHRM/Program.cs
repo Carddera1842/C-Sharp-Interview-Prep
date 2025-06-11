79684262cb5f781adece4213f83a2c183f2cc903
Carddera1842 <carddera1842@gmail.com>
2025-06-11 10:42:10 -0500

    Demo: Working with Strings

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -2,22 +2,6 @@
 
 using BethanysPieShopHRM;
 
-int amount = 1234;
-int months = 12;
-int bonus = 1000;
-
-//int yearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
-
-//Console.WriteLine($"Yearly wage: {yearlyWage}");
-
-//Console.ReadLine();
-
-//double amountDouble = 1500.0;
-//double monthsDouble = 12;
-//double bonusDouble = 1000;
-
-//double yearlyWageDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
-
-Utilities.UsingOptionalParamenters();
+Utilities.ManipulatingStrings();
 
 Console.ReadLine();
--- BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
@@ -9,6 +9,29 @@ namespace BethanysPieShopHRM
     internal class Utilities
     {
 
+        public static void ManipulatingStrings()
+        {
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification = String.Concat(firstName, lastName);
+        
+            string empId = firstName.ToLower() + " " + lastName.Trim().ToLower();
+
+            int length = empId.Length;
+
+            if (fullName.Contains("beth") || fullName.Contains("Beth"))
+            {
+                Console.WriteLine("It's Bethany!");
+            }
+
+            string subString = fullName.Substring(1, 3);
+            Console.WriteLine("Characters 2 to 4 of fullName are " + subString);
+
+            string userNameWithInterpolation = $"{firstName}-{lastName}";
+        }
+
         public static void UsingSimpleStrings()
         {
             string firstName = "Bethany";
