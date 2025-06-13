3229e8bc0ff8fce9b1339c6ccaa8a69f26d09360
Carddera1842 <carddera1842@gmail.com>
2025-06-13 11:48:14 -0500

    Testing Exceptions with MSTest V2

---
+++ Error Handling in C# By Jason Roberts/Getting Started with Exceptions/ConsoleCalculator.Test.MSTest/Test1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+
+namespace ConsoleCalculator.Test.MSTest;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void ThrowWhenUnsupportedOperation()
+    {
+        var sut = new Calculator();
+
+        //Assert.ThrowsException<CalculationOperationNotSupportedException>
+        //    (() => sut.Calculate(1, 1, "+"));
+
+        var ex = Assert.ThrowsException<CalculationOperationNotSupportedException>
+            (() => sut.Calculate(1, 1, "+"));
+
+        Assert.AreEqual("+", ex.Operation);
+    }
+}
