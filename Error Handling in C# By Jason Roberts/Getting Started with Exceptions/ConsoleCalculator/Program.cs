bfe3bed0f9174e99df995fa3e76287fbe899a2ad
Carddera1842 <carddera1842@gmail.com>
2025-06-13 10:24:32 -0500

    Catching, Throwing, and Rethrowing Exceptions

--- Error Handling in C# By Jason Roberts/Getting Started with Exceptions/ConsoleCalculator/Program.cs
+++ Error Handling in C# By Jason Roberts/Getting Started with Exceptions/ConsoleCalculator/Program.cs
@@ -3,6 +3,11 @@ using static System.Console;
 
 // Note: Additional input validation omitted for brevity
 
+AppDomain currentAppDomain = AppDomain.CurrentDomain;
+currentAppDomain.UnhandledException +=
+    new UnhandledExceptionEventHandler(HandleException);
+
+
 WriteLine("Enter first number");
 int number1 = int.Parse(ReadLine()!);
 
@@ -19,15 +24,39 @@ try
     int result = calculator.Calculate(number1, number2, operation);
     DisplayResult(result);
 }
+catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+{
+    //Log.Error(ex)
+    WriteLine($"Operation was not proided. {ex}");
+}
+catch (ArgumentNullException ex)
+{
+    //Log.Error(ex);
+    WriteLine($"An argument was null. {ex}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    //Log.Error(ex);
+    WriteLine($"Operation is not supported. {ex}");
+}
 catch (Exception ex)
 {
     WriteLine($"Sorry, something went wrong. {ex}");
+}
+finally
+{
+    WriteLine("Finally");
 }
 
 
 
 WriteLine("\nPress enter to exit.");
 ReadLine();
-    
+static void DisplayResult(int result) => WriteLine($"Result is: " +
+    $"{result}");
 
-static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+void HandleException(object sender, UnhandledExceptionEventArgs e)
+{
+
+    WriteLine($"Sorry there was aproblem and we need to close. Details: {e.ExceptionObject}");
+}
